6080c93339acbe59f496e1a93874cb3af055d4ff
wawi-m <122799427+wawi-m@users.noreply.github.com>
2023-03-28 16:25:55 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,13 +2,13 @@
 
 /**
  * _printf - Printf function
- * @format: format
- *
+ * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
 
@@ -29,7 +29,14 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			printed = handle_print(format, &i, list, buffer);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -43,6 +50,18 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
 
 /**
  * print_buffer - Prints the contents of the buffer if it exist
